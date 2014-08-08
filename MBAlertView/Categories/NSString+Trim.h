059af9f9f16cb4cedd1fc96782adc3c4434b8e91
Matthew Leibowitz <mattleibow@live.com>
2014-08-08 02:22:12 +0200

    Added the UIKit import for library builds without the pch

--- MBAlertView/Categories/NSString+Trim.h
+++ MBAlertView/Categories/NSString+Trim.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NSString (Trim)
 - (NSString*)stringByTruncatingToSize:(CGSize)size withFont:(UIFont*)font addQuotes:(BOOL)addQuotes;
